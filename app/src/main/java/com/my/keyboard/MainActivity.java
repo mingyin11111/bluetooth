d51c27c7d667cf0f30eea447b75b7b7a214dbbbb
mingyin11111 <162733135+mingyin11111@users.noreply.github.com>
2024-05-17 11:13:11 +0800

    ss

--- app/src/main/java/com/my/keyboard/MainActivity.java
+++ app/src/main/java/com/my/keyboard/MainActivity.java
@@ -1,6 +1,5 @@
 package com.my.keyboard;
 
-
 import android.bluetooth.BluetoothHidDevice;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothProfile;
@@ -10,6 +9,23 @@ import android.util.Log;
 
 import android.os.Bundle;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothHidDevice;
+import android.bluetooth.BluetoothHidDeviceAppQosSettings;
+import android.bluetooth.BluetoothHidDeviceAppSdpSettings;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.provider.SyncStateContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -18,6 +34,10 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+
+
+
 
 import com.my.keyboard.databinding.ActivityMainBinding;
 
@@ -62,9 +82,9 @@ public class MainActivity extends AppCompatActivity {
                         Log.e("tag" ,   "Proxy received but it's not  BluetoothHIDDevice");
                             return;
                     }
-                    花木成畦手自栽
-                    mHidDevice=(BluetoothHidDevice) proxy;
 
+                    mHidDevice=(BluetoothHidDevice) proxy;
+                    registerBluetoothHid();
                 }
             }
         }
@@ -76,5 +96,46 @@ public class MainActivity extends AppCompatActivity {
     },BluetoothProfile.HID_DEVICE);
 
     }
+
+    private void registerBluetoothHid() {
+        if (mHidDevice == null) {
+            Log.e(TAG, "hid device is null");
+            return;
+        }
+
+        mHidDevice.registerApp(mouseSdpSettings, null, qosSettings, Executors.newCachedThreadPool(), new BluetoothHidDevice.Callback() {
+            @Override
+            public void onAppStatusChanged(BluetoothDevice pluggedDevice, boolean registered) {
+                Log.d(TAG, "onAppStatusChanged:" + (pluggedDevice != null ? pluggedDevice.getName() : "null") + " registered:" + registered);
+                if (registered) {
+                    Log.d(TAG, "paired devices: " + mHidDevice.getConnectionState(pluggedDevice));
+                    if (pluggedDevice != null && mHidDevice.getConnectionState(pluggedDevice) != BluetoothProfile.STATE_CONNECTED) {
+                        boolean result = mHidDevice.connect(pluggedDevice);
+                        Log.d(TAG, "hidDevice connect:" + result);
+                    }
+                }
+                if (mBluetoothHidStateListener != null) {
+                    mBluetoothHidStateListener.onRegisterStateChanged(registered, pluggedDevice != null);
+                }
+            }
+
+            @Override
+            public void onConnectionStateChanged(BluetoothDevice device, int state) {
+                Log.d(TAG, "onConnectionStateChanged:" + device + "  state:" + state);
+                if (state == BluetoothProfile.STATE_CONNECTED) {
+                    mHostDevice = device;
+                }
+                if (state == BluetoothProfile.STATE_DISCONNECTED) {
+                    mHostDevice = null;
+                }
+                if (mBluetoothHidStateListener != null) {
+                    mBluetoothHidStateListener.onConnectionStateChanged(state);
+                }
+            }
+        });
+    }
+
+
+
 
 }
